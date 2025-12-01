ec2680ab3b8a6b51b3e115781754aa7c7c8e0316
wzc520pyfm <1528857653@qq.com>
2025-12-01 23:53:39 +0800

    refactor: switch from createRoot to ReactDOM.render for compatibility with React 17

--- packages/playground/react17-webpack4/src/main.jsx
+++ packages/playground/react17-webpack4/src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './style.scss'
@@ -8,10 +8,12 @@ import './style.scss'
 // 注意：React Scan 现在通过 webpack.config.js 自动注入
 
 // React 17 uses ReactDOM.render (not createRoot which is React 18+)
-createRoot(document.getElementById('root')).render(
+// eslint-disable-next-line react-dom/no-render
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
+  document.getElementById('root'),
 )
